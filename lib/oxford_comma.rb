5ab8c3642de5c8a5d25c4564ea4baf35d48ddcd8
srz2 <srz222@gmail.com>
2015-05-15 09:05:11 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  index = 0
+  output = ""
+  array.each do |word|
+    if index == array.length - 1
+      if array.length == 1
+        output += word
+      else
+        output += "and " + word
+      end
+    else
+      if array.length == 2
+        output += word + " "
+      else
+        output += word + ", "
+      end
+    end
+    index += 1
+  end
+  output
 end
