41c65b7ade6304e0a3ab55855b7eb2fa2127bb35
Linus Liang <linus.liang@nyu.edu>
2015-05-16 13:01:22 -0400

    finished oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+	str = ""
+	ct = 0
+	if array.length==1
+		return array.shift
+	elsif array.length == 2
+		return "#{array[0]} and #{array[1]}"
+	end
+	while ct < array.length-1
+		str += "#{array[ct]}, "
+		ct+=1
+	end
+	str+="and #{array[ct]}"
+	return str
 end
