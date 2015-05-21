29aabe53f07e8a333e5ad7b5a7a99be1ce5be299
gjc2118 <gjc2118@gmail.com>
2015-05-20 18:21:06 -0700

    and

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  result = ""
+  if array.length == 1
+    result << array.shift
+  elsif array.length == 2
+    result << array.shift + " and " + array.shift
+  else
+    while array.length > 1
+      result << array.shift
+      result << ", "
+    end
+    result << "and "
+    result << array.shift
+  end
 end
