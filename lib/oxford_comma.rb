3a721b588f11e2c896c8b4f181806a0f39cbaf52
chaplonglau <chaplonglau@gmail.com>
2015-05-29 19:22:38 -0700

    comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  s=""
+
+  if array.length==1
+    s=s+array[0]
+  elsif array.length==2
+    s=s+array[0]+" and "+array[1]
+  else array.length>=3
+    s=s+array[0]+", "
+    for i in 1..array.length-2
+      s=s+array[i]+", " 
+    end
+    s=s+"and "+array[array.length-1]
+  end
+  return s
 
 end
