45de82873741fd0f6e7440cd58007cdde1c4bc9b
Rsenger <Rgermosen12@gmail.com>
2015-06-09 11:21:32 -0400

    Oxford

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+	
+	if array.length == 1 
+	
+		array * ""
+	
+	elsif array.length == 2
+	
+		array * " and "
+	
+	elsif array.length >= 3
+
+		final = array.pop
+		array * ", " + ", and " + final
+	
+	end
 
 end
