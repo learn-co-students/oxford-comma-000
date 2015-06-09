caca17240ccfdb0c8342e2729bacecffb0ad3484
aeremin <aeremin@maxus.ru>
2015-06-09 16:34:29 +0300

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size <= 1 then
+    return array.join
+  elsif array.size == 2 then
+    return array.join(" and ")  
+  else	
+    last_element = array.pop
+	return array.join(", ") + ", and " + last_element
+  end	
 end
