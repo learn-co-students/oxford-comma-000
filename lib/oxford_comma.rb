d6dbb232425bb1015c8317cc77129bdf767690df
wolfwzrd <tatislois@icloud.com>
2015-06-16 15:08:44 -0400

    :)

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+	if array.size <= 1
+		array.join
+	elsif array.size == 2
+		array.join(" and ")
+	else 
+		last = array.pop
+		array.join(", ") + ", and " + last
+	end
+
+# able to call/manipulate an array directly from the methhod argument.. no need to << into a new array
+# pop calls the last element in a array... it does not pop it ( remove it )
+# join returns every element in a array in the form of one string - [ "a", "b", "c" ].join        #=> "abc"
+	
 
 end
