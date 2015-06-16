4bc94cdcd022425fa25de9cecbc6764cddff4618
Rigz777 <rigzgoodmusic@gmail.com>
2015-06-16 14:25:35 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  string = ""
 
+  if array.length == 1
+    string = string + array[0]
+
+  elsif array.length == 2
+    string = string + array[0] + " and " + array[1]
+
+  elsif array.length >= 3
+    string = string + array[0] + ", "
+      for i in 1..array.length - 2
+        string = string + array[i] + ", "
+    end
+    string = string + "and " + array[array.length - 1]
+  end
+  string
 end
