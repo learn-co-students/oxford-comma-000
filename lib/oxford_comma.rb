d47c4fabcd2f67f973a2ea6ddf28e74ce189874d
brianduggan <brian.e.duggan@icloud.com>
2015-06-20 12:03:19 -0400

    add oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+my_test = ["apple", "orange", "banana", "lemon"]
 
+def oxford_comma(array)
+  last = array.pop
+  if array.length > 1
+    final_array = array.join(", ")
+    final_array += ", and " + last
+  elsif array.length == 1
+    final_array = array.join()
+    final_array += " and " + last
+  else
+    last
+  end
 end
