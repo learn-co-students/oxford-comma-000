b40d96802d892eea6865bd6e03c5a1029087da74
mbrickenworthe <adamwinters456@gmail.com>
2015-06-24 19:20:39 -0400

    Done?

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  arrayLength = array.length
+  if arrayLength == 1
+    return array.join
+  end
+
+  if arrayLength == 2
+    array.insert(-2, "and")
+    return array.join(" ")
+  end
+
+
+  if arrayLength > 2
+      lastItem = array.pop.to_s
+      lastItem = ("and " + lastItem)
+      array.push(lastItem) 
+      return array.join(", ")
+  end
 
 end
