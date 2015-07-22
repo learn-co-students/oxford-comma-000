36275e2a0c1dcc9b31e16f19bdec785f53dc4f9a
markedwardmurray <markedwardmurray@gmail.com>
2015-07-22 15:06:42 -0400

    mark's solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  end
+  if array.size >= 2
+    array[array.size - 1] = "and " + array[array.size - 1]
+  end
+  if array.size == 2
+    array.join(" ")
+  elsif array.size >= 3
+    array.join(", ")
+  end
 end
