de0f3a8386c4fd485808e0f7a99231bf6fd02803
Helga108 <helgamir@gmail.com>
2015-09-21 23:03:35 +0300

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    element = array.pop
+    string = array.join(", ")
+    string += ", and " + element
+    string
+  end
+      
 end
