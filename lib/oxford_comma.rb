9fe74a017b89e210ee35187f751af28ede0b7e9a
briankil <brian.kil@gmail.com>
2015-11-05 13:36:15 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+	last = array.pop
+	if array.length > 1
+		array.join(", ") + ", and " + last
+	elsif array.length == 1
+		array.join(" ") + " and " + last
+	else last
+	end
 end
