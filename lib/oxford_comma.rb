890eb65632decf64258a24f6c37c8b1c6087998f
Robert Horrigan <roberthorrigan@gmail.com>
2016-05-18 23:50:50 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    output = ""
+    array.each_with_index.map do |elem, i|
+      if i == array.length - 1
+        output.concat('and ' + elem)
+      elsif i == array.length 
+        output.concat(elem)
+      else  
+        output.concat(elem + ', ')
+      end  
+    end 
+    output
+  end
 end
