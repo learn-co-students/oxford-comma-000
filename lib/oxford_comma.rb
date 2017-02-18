6eeb101e94ed89c2216d80539f04586ba7da1dac
tarikfayad <tarikfayad@gmail.com>
2017-02-18 18:10:42 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    case array.length
+        when 1
+            return array.first
+        when 2
+            return array.join(" and ")
+        else 
+            list_string = ""
+            array.each_with_index do |string, index|
+                if index + 1 < array.length
+                    list_string += "#{string}, "
+                else
+                    list_string += "and #{string}"
+                end
+            end
+            
+            return list_string
+        end
 end
